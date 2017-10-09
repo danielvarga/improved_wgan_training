4332055c04665b29f5c53f2bc67a121d20257872
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-10-09 17:35:14 +0200

    learning rate should be small if there is no LR decay

--- grid_trainsize_mnist_nobn_with_gp.sh
+++ grid_trainsize_mnist_nobn_with_gp.sh
@@ -5,5 +5,5 @@ mkdir -p couts/
 cp classifier.py couts/$NAME.classifier.py
 for VAL in `seq 100 100 900` `seq 1000 1000 10000`
 do
-	CUDA_VISIBLE_DEVICES=0 python classifier.py --DO_BATCHNORM=False --LAMBDA=0.0001 --LIPSCHITZ_TARGET=0.3 --ITERS=10000 --DATASET=mnist --DISC_TYPE=conv --$VAR=$VAL > couts/$NAME.${VAR}_${VAL}.cout 2> couts/$NAME.${VAR}_${VAL}.cerr
+	CUDA_VISIBLE_DEVICES=0 python classifier.py --DO_BATCHNORM=False --LAMBDA=0.0001 --LIPSCHITZ_TARGET=0.3 --ITERS=5000 --DATASET=mnist --DISC_TYPE=conv --$VAR=$VAL > couts/$NAME.${VAR}_${VAL}.cout 2> couts/$NAME.${VAR}_${VAL}.cerr
 done
