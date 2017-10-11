f2dc463978047d39e0a4d0d77afb6763cfbc1760
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-10-11 22:55:55 +0200

    bugfix on grid_trainsize_mnist_gsgp.sh

--- grid_trainsize_mnist_gsgp.sh
+++ grid_trainsize_mnist_gsgp.sh
@@ -5,5 +5,5 @@ mkdir -p couts/
 cp classifier.py couts/$NAME.classifier.py
 for VAL in `seq 100 100 900` `seq 1000 1000 10000`
 do
-	CUDA_VISIBLE_DEVICES=0 python classifier.py --GRADIENT_SHRINKING=True--LAMBDA=0.0001 --DO_BATCHNORM=False --TRAIN_DATASET_SIZE=500 --DATASET=mnist --DISC_TYPE=conv --$VAR=$VAL > couts/$NAME.${VAR}_${VAL}.cout 2> couts/$NAME.${VAR}_${VAL}.cerr
+	CUDA_VISIBLE_DEVICES=0 python classifier.py --GRADIENT_SHRINKING=True --LAMBDA=0.0001 --DO_BATCHNORM=False --DATASET=mnist --DISC_TYPE=conv --$VAR=$VAL > couts/$NAME.${VAR}_${VAL}.cout 2> couts/$NAME.${VAR}_${VAL}.cerr
 done
