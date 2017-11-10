bd562079c0a8fb3780b501195b87ea975249de0e
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-11-10 11:00:28 +0100

    stat.py needs a max_steps argument, type specification moved to separate file stat_type.py, but that needs more work

--- stat.py
+++ stat.py
@@ -17,11 +17,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("rootdir", help="Folder containing the logs")
 parser.add_argument("z_key", help="Key for z axis")
 parser.add_argument("regexp", type=str, help="Only regexp matched folder names will be parsed")
+parser.add_argument("MAX_STEP", type=int, help="Discard runs that did not run until MAX_STEP iterations")
 args = parser.parse_args()
+
+import stat_type
 
 rootdir = args.rootdir
 z_key = args.z_key
 regexp_to_match = args.regexp
+MAX_STEP = args.MAX_STEP
 
 keys_from_filepath = ["lambda", "gp", "gs", "lr", "net", "iters", "train", "wd", "lips", "combslopes", "lrd", "aug", "bs", "bn", "dg", "comb", "ent"]
 
@@ -55,92 +59,13 @@ for folder, subs, files in os.walk(rootdir):
                 val = match.group('val')
                 record[key] = float(val) if represents_float(val) else val
 
-        if record['train'] < 500: # to make the results on the plot more visible
+
+        type = stat_type.get_type(record)
+        if type == None:
             continue
-
-        
-                
-        if False: # comparing datagrad parameters with batchnorm
-            if record['dg'] == 0 or record['bn'] != "y":
-                continue
-            type = "dg_bn_" + str(record['dg'])
-        elif False: # comparing datagrad parameters with dropout
-            if record['dg'] == 0 or record['bn'] == "y":
-                continue
-            type = "dg_do_" + "%05.2f" % record['dg']
-        elif False: # comparing datagrad bn with datagrad do
-            if record['dg'] == 0:
-                continue
-            type = "dg_bn_" + str(record['bn'])
-        elif False: # comparing unreg dropout and batchnorm
-            if record['dg'] != 0 or record['lambda'] != 0 or record['gs'] == 'y':
-                continue
-            type = "unreg_bn_" + record['bn']
-        elif False: # comparing GP with L2 gradient loss bn vs dropout
-            if record['dg'] != 0 or record['lambda'] == 0 or record['gs'] != 'n' or record['comb'] != "random" or record['gp'] != 3:
-                continue
-            type = "gp3a_bn_" + record['bn']
-        elif False: # comparing GP with L2 gradient loss with dropout for various lambdas
-            if record['dg'] != 0 or record['lambda'] == 0 or record['gs'] != 'n' or record['comb'] != "random" or record['gp'] != 3 or record['bn'] != "n":
-                continue
-            type = "gp3a_do_" + "%06.4f" % record['lambda']
-        elif False: # comparing GP with softmax bn vs dropout
-            if record['dg'] != 0 or record['lambda'] == 0 or record['gs'] != 'n' or record['comb'] != "softmax" or record['gp'] != 3:
-                continue
-            type = "gp4_bn_" + record['bn']
-        elif False: # comparing GP with softmax for various lambdas
-            if record['dg'] != 0 or record['lambda'] == 0 or record['gs'] != 'n' or record['comb'] != "softmax" or record['gp'] != 3:
-                continue
-            type = "gp4_" + "%06.4f" % record['lambda']
-        elif False: # comparing GP with L2 gradient loss pushing gradients into LIPSCHITZ_TARGET bn vs dropout
-            if record['dg'] != 0 or record['lambda'] == 0 or record['gs'] != 'n' or record['comb'] != "random" or record['gp'] != 4:
-                continue
-            type = "gp3b_bn_" + record['bn']
-        elif False: # comparing GP with L2 gradient loss pushing gradients into LIPSCHITZ_TARGET with dropout for various lipschitz_targets
-            if record['dg'] != 0 or record['lambda'] == 0 or record['gs'] != 'n' or record['comb'] != "random" or record['gp'] != 4  or record['bn'] != "n":
-                continue
-            if record['lips'] > 2:
-                continue
-            type = "gp3b_do_" + "%05.2f" % record['lips']
-        elif False: # comparing GP with L2 gradient loss pushing gradients into LIPSCHITZ_TARGET with dropout for LIPS=0.7 various lambdas
-            if record['dg'] != 0 or record['lambda'] == 0 or record['gs'] != 'n' or record['comb'] != "random" or record['gp'] != 4  or record['bn'] != "n" or record['lips'] != 0.7:
-                continue
-            type = "gp3b_do_lips_0.7_" + "%06.4f" % record['lambda']
-        elif False: # final comparison
-            if record['dg'] == 0 and record['lambda'] == 0 and record['gs'] == 'n' and record['bn'] == 'n':
-                type = "1_unreg"
-            elif record['dg'] == 10 and record['bn'] == "n":
-                type = "2_datagrad"
-            elif record['dg'] == 0 and record['lambda'] == 0.01 and record['comb'] == "random" and record['gp'] == 3 and record['bn'] == 'n':
-                type = "3a_gp_to_zero"
-            elif record['dg'] == 0 and record['lambda'] == 0.01 and record['comb'] == "random" or record['gp'] == 4  and record['bn'] == "n" and record['lips'] == 0.7:
-                type = "3b_gp_to_lips"
-            elif record['dg'] == 0 and record['lambda'] == 0.1 and record['comb'] == "softmax" and record['gp'] == 3:
-                type = "4_softmax"
-            else:
-                continue
-        elif True: # final comparison
-            type = "lenet"
-            type += "_comb" + str(record['comb'])
-            type += "_lambda" + str(record['lambda'])
-            type += "_ent" + str(record['ent'])
-            type += "_bn" + str(record['bn'])
-            type += "_wd" + str(record['wd'])
-            type += "_net" + str(record['net'])
-
         else:
-            type = "unknown"
-            if record['lambda'] == 0 and 'dg' in record  and  record['dg'] == 0:
-                type = 'nogp'
-            elif record['lambda'] != 0 and record['gp'] == 2:
-                type = 'gp'
-            elif record['lambda'] == 0 and 'dg' in record and record['dg'] != 0:
-                type = 'datagrad'
-            elif record['lambda'] == 0 and record['gs'] == 'y':
-                type = 'gs'
-                
-        print(folder)
-        record['type'] = type
+            print(folder)
+            record['type'] = type
 
         ea = event_accumulator.EventAccumulator(logfile)
         ea.Reload()
@@ -153,7 +78,7 @@ for folder, subs, files in os.walk(rootdir):
         for z in zs:
             max_step = z.step
             z_vals.append(z.value)
-        if max_step < 10000:
+        if max_step < MAX_STEP:
             continue
 #        print(z_vals)
         record[z_key] = z_vals[-1]
