35708a29a0e598f0222afbd5175cc47fc5421723
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-11-12 12:03:50 +0100

    check entropy penalty weight for a large range of trainsizes

--- paper1/grid_mnist_ent.sh
+++ paper1/grid_mnist_ent.sh
@@ -2,22 +2,27 @@
 
 NAME=mnist_ent
 mkdir -p couts/
+echo $NAME
 
 D=0
 WD=0.0005
-TRAIN=2000
 LRD=piecewise
-NET=lenet
-BN=False
 
 for C in `seq 1 1 10`
 do
     echo ITER $C
-	COMMON_ARGS="--RANDOM_SEED=$C --LEARNING_RATE_DECAY=$LRD --WEIGHT_DECAY=$WD --DISC_TYPE=$NET --TRAIN_DATASET_SIZE=$TRAIN --DO_BATCHNORM=$BN"
-	
-	for EP in 0.001 0.003 0.01 0.03 0.1 0.3 
+	for NET in lenet lenettuned
 	do
-		CUDA_VISIBLE_DEVICES=$D python classifier.py $COMMON_ARGS --ENTROPY_PENALTY=$EP > couts/$NAME.do_EP_${EP}_${C}.cout 2> couts/$NAME.do_EP_${EP}_${C}.cerr
+		echo NET $NET
+		for TRAIN in 500 1000 2000 3000 4000 5000 10000 15000 20000
+		do
+			COMMON_ARGS="--RANDOM_SEED=$C --LEARNING_RATE_DECAY=$LRD --WEIGHT_DECAY=$WD --DISC_TYPE=$NET --TRAIN_DATASET_SIZE=$TRAIN"
+	
+			for EP in 0.001 0.003 0.01 0.03 0.1 0.3 
+			do
+				CUDA_VISIBLE_DEVICES=$D python classifier.py $COMMON_ARGS --ENTROPY_PENALTY=$EP > couts/$NAME.do_EP_${EP}_NET_${NET}_${C}.cout 2> couts/$NAME.do_EP_${EP}_NET_${NET}_${C}.cerr
+			done
+		done
 	done
 done
 
