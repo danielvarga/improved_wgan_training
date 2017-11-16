130379f1f7ee9567269c2ab37b274eb937b14ad1
Zsolt Zombori <zsolt.zombori@t-online.hu>
2017-11-16 16:44:45 +0100

    small changes to stat_type.py

--- stat_type.py
+++ stat_type.py
@@ -15,6 +15,10 @@ def get_type(record, type_grouping):
         return types_gp_lenet(record)
     elif type_grouping == "gp_lenettuned":
         return types_gp_lenettuned(record)
+    elif type_grouping == "jacreg_lenet":
+        return types_jacreg_lenet(record)
+    elif type_grouping == "jacreg_lenettuned":
+        return types_jacreg_lenettuned(record)
 
     return compare_mnist_1(record)
 
@@ -26,6 +30,7 @@ def basic_types(record):
     type += "-ent_" + str(record['ent'])
     type += "-bn_" + str(record['bn'])
     type += "-wd_" + str(record['wd'])
+    return type
 
 # for visualizing grid_mnist_datagrad.sh results for lenet
 # python stat.py /mnt/g2big/tensorboard_logs/paper1/mnist_datagrad accuracy test 10000 -type_grouping datagrad_lenet
@@ -82,6 +87,26 @@ def types_gp_lenettuned(record):
         return None
     elif record['lambda'] >= 0.1 or record['lambda'] <= 0.0001:
         return None
+    else:
+        return "lambda %06.4f" % record['lambda']
+
+# for visualizing grid_mnist_jacreg.sh results for lenet
+# python stat.py /home/zombori/working_shadow/logs accuracy test 10000 -type_grouping jacreg_lenet
+def types_jacreg_lenet(record):
+    if record['net'] != "lenet":
+        return None
+#    elif record['lambda'] >= 0.1 or record['lambda'] <= 0.0001:
+#        return None
+    else:
+        return "lambda %06.4f" % record['lambda']
+
+# for visualizing grid_mnist_jacreg.sh results for lenettuned
+# python stat.py /home/zombori/working_shadow2/logs accuracy test 10000 -type_grouping jacreg_lenettuned
+def types_jacreg_lenettuned(record):
+    if record['net'] != "lenettuned":
+        return None
+#    elif record['lambda'] >= 0.1 or record['lambda'] <= 0.0001:
+#        return None
     else:
         return "lambda %06.4f" % record['lambda']
 
