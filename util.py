ee7971d3aa9f9499f9d50aeccd6da90ac84cedcc
Zsolt Zombori <zsolt.zombori@t-online.hu>
2018-05-07 16:45:41 +0200

    implemented super convergence: LEARNING_RATE_DECAY=linear

--- util.py
+++ util.py
@@ -3,6 +3,10 @@ import tensorflow as tf
 import pandas as pd
 import seaborn as sns
 
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import control_flow_ops
+
 # input: discriminator and a set of points
 # find the greatest slopes in the neighbourhood of the points
 # return the greatest slopes and the corresponding points
@@ -75,3 +79,27 @@ def get_weight_loss(weights, wd):
     # return weight_loss
 
     #   scope = tf.variable_scope('weights_norm')
+
+# implement piecewise linear (learning rate) schedule
+def piecewise_linear_schedule(global_step, boundaries, values, name=None):
+    if global_step is None:
+        raise ValueError("global_step is required for piecewise_linear_schedule.")
+    assert len(boundaries) == len(values), "boundaries length ({}) should equal values length ({})".format(len(boundaries), len(values))
+
+    with ops.name_scope(name, "piecewise_linear_schedule",
+                      [global_step, boundaries, values]) as name:
+        x = math_ops.cast(global_step, tf.float32)
+        pred_fn_pairs = []
+        pred_fn_pairs.append((x <= boundaries[0], lambda: values[0]))
+        pred_fn_pairs.append((x > boundaries[-1], lambda: values[-1]))
+        for low, high, low_v, high_v in zip(boundaries[:-1], boundaries[1:], values[:-1], values[1:]):
+            # Need to bind v here; can do this with lambda v=v: ...
+            pred = (x > low) & (x <= high)
+            r = (x - low) / (high - low)
+            v = r * high_v + (1-r) * low_v
+            pred_fn_pairs.append((pred, lambda v=v: v))
+
+        # The default isn't needed here because our conditions are mutually
+        # exclusive and exhaustive, but tf.case requires it.
+        default = lambda: values[0]
+        return control_flow_ops.case(pred_fn_pairs, default, exclusive=True)
